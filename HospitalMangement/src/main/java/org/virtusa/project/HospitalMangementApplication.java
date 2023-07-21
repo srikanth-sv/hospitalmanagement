59f9f8ccaeaba25b216538090e47fa6a61377615
srikanth-svg <137396968+srikanth-svg@users.noreply.github.com>
2023-07-21 11:53:15 +0530

    First Commit Added Project

---
+++ HospitalMangement/src/main/java/org/virtusa/project/HospitalMangementApplication.java
@@ -0,0 +1,13 @@
+package org.virtusa.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalMangementApplication.class, args);
+	}
+
+}
